5aa470622d37aec247f24d71aecdeeb096a4c187
FelipeKondoNakano <nakano.felipe02@gmail.com>
2025-02-21 16:18:13 -0300

    feat.Atividade da Semana 2 realizada

--- Semana_2/Atividade_3/script.js
+++ Semana_2/Atividade_3/script.js
@@ -1,3 +1,13 @@
-document.getElementById('button').addEventListener("click", () => {
+document.getElementById("button").addEventListener("click", () => {
+    const texto = document.getElementById('texto').value.toLowerCase();
+    const palavras = texto.toLowerCase().split(" "); // quebra a frase em palavras
+   
+    const palavrasInvertidas = palavras.map(palavra =>
+        palavra.replace(/[\W_]/g, '').split('').reverse().join('') // inverte cada palavra e junta depois
+    );
+ 
+    const textoInvertido = palavrasInvertidas.join(' ').toLowerCase();
     
-})
+    let ehPalindromo = texto.toLowerCase().replace(/[\W_]/g, '') === textoInvertido.replace(/[\W_]/g, '')
+    alert(ehPalindromo ? "A frase é um palíndromo!" : "A frase não é um palíndromo!");
+});
